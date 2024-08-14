80ab62c16771f2733924f087194e04884876277e
ryli1120 <77474304+ryli1120@users.noreply.github.com>
2024-08-13 23:50:06 -0400

    Create intro_practice.py

---
+++ intro_practice.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+number = 17
+name = "Ryan"
+
+names_List = ["John", "Jeff", "Jesse", name]
+
+Animals = {
+ "Mammals": ["dog", "cat", "horse"],
+ "Reptiles":{
+   "Snakes": ["python", "cobra", "rattlesnake"]
+ },
+  "Amphibian": "Frog"
+}
+
+def checkBMI(weight, height):
+  bmi = weight/(height **2)
+
+if bmi < 18.5:
+  return("Your BMI is " + bmi + ". You are underweight.")
+elif 18.5<= bmi < 24.9:
+  return ("Your BMI is " + bmi + ". You are at a health weight.")
+elif 25 <= bmi 29.9:
+  return ("Your BMI is " + bmi + ". You are at a overweight.")
+else:
+  return ("Your BMI is " + bmi + ". You are obese.")
